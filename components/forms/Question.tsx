0ef2971c57bc41ee83b07866c2adf79e00f3a2f9
mo-amininasab <mo.amininasabb@gmail.com>
2023-12-25 12:55:38 +0330

    add server action to send question to db

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -20,12 +20,20 @@ import Image from "next/image";
 import { Editor } from "@tinymce/tinymce-react";
 import { Badge } from "../ui/badge";
 import { createQuestion } from "@/lib/actions/question.action";
+import { useRouter, usePathname } from 'next/navigation';
+
+interface Props {
+  mongoUserId: string;
+}
 
 const type: any = "create";
 
-const Question = () => {
+const Question = ({mongoUserId}: Props) => {
   const editorRef = useRef(null);
   const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const router = useRouter();
+  const pathname = usePathname();
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof QuestionsSchema>>({
@@ -45,9 +53,15 @@ const Question = () => {
       // make an async call to your API -> create a question
       // contain all form data
 
-      await createQuestion({});
+      await createQuestion({
+        title: values.title,
+        content: values.explanation,
+        tags: values.tags,
+        author: JSON.parse(mongoUserId),
+      });
 
       // navigate to home page
+      router.push('/');
     } catch (error) {
       
     } finally {
--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -11,7 +11,7 @@ export const connectToDatabase = async () => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
-      dbName: "Project 0",
+      dbName: "devflow",
     });
 
     isConnected = true;
