f2d4940a6b2c4e7b628ebe8f36d511ffee524e50
mo-amininasab <mo.amininasabb@gmail.com>
2023-12-31 19:27:41 +0330

    implement: TagsPage

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -2,7 +2,8 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
-import { GetTopInteractedTagsParams } from "./shared.types";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
+import Tag, { ITag } from "@/database/tag.model";
 
 export async function getTopInteractedTags({
   userId,
@@ -18,10 +19,28 @@ export async function getTopInteractedTags({
     // We'll create Interaction model later
 
     return [
-      { _id: '1', name: "tag1" },
-      { _id: '2', name: "tag2" },
-      { _id: '3', name: "tag3" },
+      { _id: "1", name: "tag1" },
+      { _id: "2", name: "tag2" },
+      { _id: "3", name: "tag3" },
     ];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAllTags({
+  page,
+  pageSize,
+  filter,
+  searchQuery,
+}: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+
+    const tags: ITag[] = await Tag.find({});
+
+    return { tags };
   } catch (error) {
     console.log(error);
     throw error;
