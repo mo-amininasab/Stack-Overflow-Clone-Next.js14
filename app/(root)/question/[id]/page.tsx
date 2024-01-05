418a6bb2a70c81c002ee8d7cbbbbf11bb2f7db74
mo-amininasab <mo.amininasabb@gmail.com>
2024-01-05 23:15:44 +0330

    initial: Vote component UI

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -42,7 +42,16 @@ const QuestionDetailPage = async ({ params, searchParams }) => {
             </p>
           </Link>
           <div className="flex justify-end">
-            <Votes />
+            <Votes 
+              type='question'
+              itemId={JSON.stringify(question._id)}
+              userId={JSON.stringify(mongoUser._id)}
+              upvotes={question.upvotes.length}
+              hasUpvoted={question.upvotes.includes(mongoUser._id)}
+              downvotes={question.downvotes.length}
+              hasDownvoted={question.downvotes.includes(mongoUser._id)}
+              hasSaved={mongoUser?.saved.includes(question._id)}
+            />
           </div>
         </div>
         <h2 className="h2-semibold text-dark200_light900 mt-3.5 w-full text-left">
