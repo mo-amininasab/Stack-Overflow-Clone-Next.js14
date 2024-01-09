7c88aa685c0caf1c2b2fc3ee80d9e411196a2c84
mo-amininasab <mo.amininasabb@gmail.com>
2024-01-09 17:31:29 +0330

    add filtering into home page

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -35,10 +35,23 @@ export async function getQuestions({
       ];
     }
 
+    let sortOptions = {};
+    switch (filter) {
+      case "newest":
+        sortOptions = { createdAt: -1 };
+        break;
+      case "frequent":
+        sortOptions = { views: -1 };
+        break;
+      case "unanswered":
+        query.answers = { $size: 0 };
+        break;
+    }
+
     const questions = await Question.find(query)
       .populate({ path: "tags", model: Tag })
       .populate({ path: "author", model: User })
-      .sort({ createdAt: -1 });
+      .sort(sortOptions);
 
     return questions;
   } catch (error) {
