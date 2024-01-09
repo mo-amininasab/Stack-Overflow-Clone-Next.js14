a653d2a7937f9b6ba1bbba7d67147288db5fb8c5
mo-amininasab <mo.amininasabb@gmail.com>
2024-01-09 18:06:02 +0330

    add filtering into collection page

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -191,11 +191,31 @@ export async function getSavedQuestions({
       query = { title: { $regex: new RegExp(searchQuery, "i") } };
     }
 
+    let sortOptions = {};
+
+    switch (filter) {
+      case "most_recent":
+        sortOptions = { createdAt: -1 };
+        break;
+      case "oldest":
+        sortOptions = { createdAt: 1 };
+        break;
+      case "most_voted":
+        sortOptions = { upvotes: -1 };
+        break;
+      case "most_viewed":
+        sortOptions = { views: -1 };
+        break;
+      case "most_answered":
+        sortOptions = { answers: -1 };
+        break;
+    }
+
     const user = await User.findOne({ clerkId }).populate({
       path: "saved",
       match: query,
       options: {
-        sort: { createdAt: -1 },
+        sort: sortOptions,
       },
       populate: [
         { path: "tags", model: Tag, select: "_id name" },
